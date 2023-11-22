7f00c5e547b3818467a6cb2f8c623221c435766d
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-22 14:29:02 +0100

    import verwijderd main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MainComponentComponent } from './main-component/main-component.component';
-import { OrderScreenComponentComponent } from './main-component/order-screen-component/order-screen-component.component';
-import { ProductsComponentComponent } from './main-component/order-screen-component/products-component/products-component.component';
-import { FilterComponentComponent } from './main-component/order-screen-component/filter-component/filter-component.component';
-import { ScanOrderComponent } from './scan-order/scan-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MainComponentComponent,
-    OrderScreenComponentComponent,
-    ProductsComponentComponent,
-    FilterComponentComponent,
   ],
   imports: [
     BrowserModule,
