1aa665ba7658b629f65dae2adf2df11346a9f398
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-22 14:15:09 +0100

    accountscomponent bij export toegevoegd

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountsComponent } from './accounts.component';
 
-
-
 @NgModule({
-  declarations: [
-    AccountsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AccountsComponent],
+  imports: [CommonModule],
+  exports: [AccountsComponent],
 })
-export class AccountsModule { }
+export class AccountsModule {}
