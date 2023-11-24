86b9215caa61c0f2fffab30a1181f2021768917a
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-24 11:48:29 +0100

    made AuthInterceptor more clean

--- src/app/login/AuthInterceptor.ts
+++ src/app/login/AuthInterceptor.ts
@@ -5,21 +5,19 @@ import { LoginService } from "./login.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  authReq:any = undefined;
   constructor(private login: LoginService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    this.authReq = req.clone()
+    if(req.url.includes('/login')){
+      return next.handle(req);
+    }
     const authToken = this.login.getJwtTOken();
-    console.log('intercerpt' + authToken.token)
-    if(authToken.token !=undefined){
 
-      this.authReq = req.clone({
+      const authReq = req.clone({
         headers: req.headers.set('Authorization', 'Bearer '+ authToken.token)
       });
-    }
-    console.log(this.authReq)
+
     // send cloned request with header to the next handler.
-    return next.handle(this.authReq);
+    return next.handle(authReq);
   }
 }
