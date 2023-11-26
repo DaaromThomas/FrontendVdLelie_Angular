0e5f9fbda28c766c19bba9d408a6997814b35173
ronp2001 <ronpaats@hotmail.com>
2023-11-26 20:10:51 +0100

    Feat: basic ability to filter on location

--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Packaging } from '../interfaces/packaging.model';
 import { HttpClient } from '@angular/common/http';
 import { DataStorageService } from '../services/data-storage.service';
+import { FormControl } from '@angular/forms';
+import { eventNames } from 'process';
 
 @Component({
   selector: 'app-stock',
@@ -12,42 +14,43 @@ import { DataStorageService } from '../services/data-storage.service';
 export class StockComponent {
   displayPackage: boolean = false;
   tableWrapperClass: string = 'table-wrapper';
+  locationFilter: string = '';
 
   stock: Packaging[] = [
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
-    new Packaging('dozen', '1', 2, 1, 'Rode Doos', 'Rotterdam'),
-    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos1', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos2', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos3', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos4', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos5', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos6', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos7', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos8', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos9', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos10', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos11', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos12', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos13', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos14', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos15', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos16', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos17', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos18', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos19', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos20', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos21', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos22', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos23', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos24', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos25', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos26', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos27', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos28', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos29', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos30', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos31', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos32', 'Amsterdam'),
+    new Packaging('dozen', '1', 2, 1, 'Rode Doos33', 'Rotterdam'),
+    new Packaging('dozen', '2', 20, 10, 'Blauwe Doos34', 'Amsterdam'),
   ];
 
   mockLocations: string[] = [
@@ -85,4 +88,11 @@ export class StockComponent {
     );
     return newPackage;
   }
+
+  setLocationFilter(location: Event) {
+    let filterValue = (event?.target as HTMLInputElement).value;
+    this.locationFilter = filterValue;
+    console.log(this.locationFilter)
+  }
+   
 }
