c6bcd1c9e6c79158c9c3bc1d05090c62b8ba907f
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-28 15:04:15 +0100

    styling grr

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LoginService } from './login.service';
 import { Login } from './login.interface';
@@ -9,12 +9,23 @@ import { Login } from './login.interface';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  showPassword: boolean = false;
+  @ViewChild('passwordInput', { static: false })
+  passwordInput!: ElementRef;
+
   login = new FormGroup({
     username: new FormControl(''),
     password: new FormControl(''),
   });
 
   constructor(private loginService: LoginService) {}
+
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+    const inputType = this.showPassword ? 'text' : 'password';
+    this.passwordInput.nativeElement.type = inputType;
+  }
 
   onClick() {
     this.loginService.loginRequest(this.login.value as Login);
