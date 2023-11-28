80cf938cbdd6cc937e76cda737e479e504cf1513
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-28 13:13:16 +0100

    added Authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ScanOrderComponent } from './scan-order/scan-order.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './login/Auth/LoginActivate';
 
 const routes: Routes = [
-  { path: 'scan-order', component: ScanOrderComponent },
-  { path: 'accounts', component: AccountsComponent },
-  {path: 'login', component: LoginComponent}
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'scan-order', component: ScanOrderComponent },
+      { path: 'accounts', component: AccountsComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -7,16 +7,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LoginService {
-  Jwttoken:any = "not logged in"
+  Jwttoken:any
   constructor(private http: HttpClient ) {
   }
 
 
   loginRequest(login: Login){
     this.http.post('http://localhost:8080/login', login).subscribe(data => this.Jwttoken = data);
-
   }
 
+  isLoggedIn():boolean{
+    if(this.Jwttoken === undefined){
+      return false;
+    }
+    return true;
+  }
   getJwtTOken(){
     return this.Jwttoken
   }
