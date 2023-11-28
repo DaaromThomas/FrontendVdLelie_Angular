606d6128563b96f26f631d4646ff5de2428b22b3
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-28 14:27:01 +0100

    on click navigate to scan order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = "appcomponent"
+
+  constructor(private loginService: LoginService){
+
+  }
+  isLoggedIn(): boolean{
+   return this.loginService.isLoggedIn()
+  }
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,21 +1,25 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Login } from './login.interface';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Login } from './login.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   Jwttoken: any;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   loginRequest(login: Login) {
     this.http
-      .post('http://localhost:8080/login', login)
-      .subscribe((data) => (this.Jwttoken = data));
+      .post('http://localhost:8080/login', login, { observe: 'response' })
+      .subscribe((res) => this.handleRes(res));
   }
-
+  handleRes(res: any) {
+    this.Jwttoken = res.body.token;
+    this.router.navigateByUrl('/scan-order');
+  }
   isLoggedIn(): boolean {
     if (this.Jwttoken === undefined) {
       return false;
