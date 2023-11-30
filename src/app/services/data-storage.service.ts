f96adc77a3a01510ff20e437867af002de207279
davidvdw02 <davidvdw02@gmail.comdnx>
2023-11-30 11:30:35 +0100

    made the models interfaces

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,38 +1,38 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { Observable } from "rxjs";
-import { Location } from "../models/location";
-import { Packaging } from "../models/packaging";
-
+import { Observable } from 'rxjs';
+import { Location } from '../models/location';
+import { Packaging } from '../models/packaging';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
-    baseurl: string = 'http://localhost:8080';
-  balls:Location[] = [];
-    constructor(private http: HttpClient) {}
+  baseurl: string = 'http://localhost:8080';
+  balls: Location[] = [];
+  constructor(private http: HttpClient) {}
 
-    storePackage(newPackage: Packaging) {
-        const httpOptions = {
-            params: new HttpParams()
-                .set('stockId', '4c491e42-46ed-4876-aa7b-4b4a10b91c32')
-                .set('name', newPackage.name)
-                .set('packagingGroup', newPackage.packagingGroup)
-                .set('amount', newPackage.amountinstock)
-                .set('minAmount', newPackage.minAmount)
-        };
+  storePackage(newPackage: Packaging) {
+    const httpOptions = {
+      params: new HttpParams()
+        .set('stockId', '4c491e42-46ed-4876-aa7b-4b4a10b91c32')
+        .set('name', newPackage.name)
+        .set('packagingGroup', newPackage.packagingGroup)
+        .set('amount', newPackage.amountinstock)
+        .set('minAmount', newPackage.minAmount),
+    };
 
-        return this.http.post(this.baseurl+"/packages", {}, httpOptions)
-            .subscribe(data => {});
-    }
+    return this.http
+      .post(this.baseurl + '/packages', {}, httpOptions)
+      .subscribe();
+  }
 
-    getPackages() {
-        return this.http.get(this.baseurl+"/packages", {});
-    }
+  getPackages() {
+    return this.http.get(this.baseurl + '/packages', {});
+  }
 
-    getLocations(){
-        return this.http.get(this.baseurl+'/locations',);
-    }
+  getLocations() {
+    return this.http.get(this.baseurl + '/locations');
+  }
 }
