3996187b7dff3ce96bd8442f452bf3c105aa35ff
DaaromThomas <56thomasp56@gmail.com>
2023-11-30 17:05:47 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,17 @@ import {ScanOrderModule} from "./scan-order/scan-order.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HttpClient } from '@angular/common/http';
 import {CommonModule} from "@angular/common";
 import { LoginModule } from './login/login.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptor } from './login/Auth/AuthInterceptor';
 import { StockModule } from "./stock/stock.module";
-import { GlobalErrorComponent } from './errors/global-error/global-error.component';
-import { ErrorInterceptor } from './errors/global-error/ErrorInterceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    GlobalErrorComponent,
+
     ],
   imports: [
     BrowserModule,
@@ -32,7 +29,6 @@ import { ErrorInterceptor } from './errors/global-error/ErrorInterceptor';
   providers: [
     provideClientHydration(),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide:HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
