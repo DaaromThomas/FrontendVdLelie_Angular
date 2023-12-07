ea68ea9a0c0303639cac4a3d198027df1a20d9c6
Ron <s1137813@student.hsleiden.nl>
2023-12-07 14:01:52 +0100

    Feature: directive to only allow numeric input

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CustomersComponent } from './customers.component';
 import { AddCustomerPopupComponent } from './add-customer-popup/add-customer-popup.component';
+import { SharedModule } from '../shared/shared-module/shared-module.module';
 
 @NgModule({
   declarations: [CustomersComponent, AddCustomerPopupComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule],
   exports: [CustomersComponent, AddCustomerPopupComponent],
 })
 export class CustomersModule {}
