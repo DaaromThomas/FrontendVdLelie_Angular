6c9a317b98101f065f859900d761aa805c0a7e60
Ruben.Colijn <s1142666@student.hsleiden.nl>
2023-12-07 09:54:09 +0100

    Fix: removed index as i in code

--- src/app/scan-order/models/selected-packaging.ts
+++ src/app/scan-order/models/selected-packaging.ts
@@ -2,8 +2,8 @@ import { Packaging } from "../../interfaces/packaging";
 
 export class SelectedPackaging {
 
-    public selectedPackaging: Packaging;
-    public amount: number;
+    selectedPackaging: Packaging;
+    amount: number;
     constructor(selectedPackaging: Packaging, amount: number) {
         this.selectedPackaging = selectedPackaging;
         this.amount = amount;
