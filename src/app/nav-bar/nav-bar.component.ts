a7fee78d691b15bdfe90b0db3d6aab8061020b99
davidvdw02 <davidvdw02@gmail.com>
2023-12-12 21:32:30 +0100

    changed to deletemapping

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CookieService } from '../login/cookie.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css'], // Use 'styleUrls' with an 's'
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
   constructor(
@@ -17,11 +17,13 @@ export class NavBarComponent {
     private loginService: LoginService
   ) {}
   onLogoutCLick() {
+   var token = this.cookieService.getCookie('refreshToken');
+let params = new HttpParams().set("refreshToken",token);
     this.http
-      .post(
-        'http://localhost:8080/refreshtoken/delete',
-        this.cookieService.getCookie('refreshToken')
-      )
+      .delete(
+        'http://localhost:8080/refreshtoken',
+        {params: params}
+      ).subscribe(data => console.log(data))
     this.router.navigateByUrl('/login');
     this.loginService.Jwttoken = undefined;
   }
