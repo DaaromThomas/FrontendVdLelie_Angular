325a3acf96e6223888f5c12d88088a9a7905eb0d
ronp2001 <ronpaats@hotmail.com>
2023-12-13 16:43:35 +0100

    fix: corrected import statement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 import { LoginModule } from './login/login.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AuthInterceptor } from './login/Auth/Auth.interceptor';
+import { AuthInterceptor } from './login/Auth/auth.interceptor';
 import { StockModule } from './stock/stock.module';
 import { CustomersModule } from './customers/customers.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,7 +23,6 @@ import { LoginService } from './login/login.service';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    ScanOrderModule,
     LoginModule,
     HttpClientModule,
     StockModule,
