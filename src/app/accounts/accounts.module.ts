a747d608ad2b59f63da2d580c6154d26c771fd26
Draakje10 <draakje11111@gmail.com>
2024-01-22 18:39:50 +0100

    Feat: Login popup now fully works only needs some styling!

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountsComponent } from './accounts.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule} from '@angular/material/checkbox'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { CreateAccountPopupComponent } from './create-account-popup/create-account-popup.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
+
 
 @NgModule({
-  declarations: [AccountsComponent, CreateAccountPopupComponent],
-  imports: [CommonModule, BrowserModule, MatTableModule, MatCheckboxModule, FormsModule, MatDialogModule, MatFormFieldModule, MatOptionModule, MatSelectModule ],
+  declarations: [AccountsComponent],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    FormsModule,
+  ],
   exports: [AccountsComponent],
 })
-export class AccountsModule {}
+export class AccountsModule { }
