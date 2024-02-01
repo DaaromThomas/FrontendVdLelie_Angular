f13faf867f0e7fc6ec767eaa5c5a4c0c8b0a6a4b
Joël <s1130735@student.hsleiden.nl>
2024-02-01 15:18:04 +0100

    test: extra cypress tests for scan-order

--- cypress/e2e/scan-order.cy.ts
+++ cypress/e2e/scan-order.cy.ts
@@ -1,43 +1,38 @@
-import { ScanOrderService } from '../../src/app/scan-order/services/scan-order.service';
-
 describe('scan-order screen tests', () => {
+
+  beforeEach(() => {
+    login();
+  });
   it('is connected', () => {
     cy.visit('https://vps.ronp.nl');
   });
 
   it('navigates to scan-order screen', () => {
-    login();
     cy.get('.title').contains('Enter Productnumber:');
   });
 
   it('should give error when not entering anything', () => {
-    login();
     cy.get('.scan-button').click();
     cy.get('.error').contains('please scan a product');
   });
 
   it('should give error when entering invalid productnumber', () => {
-    login();
     cy.get('.scan-input').first().type('ddddd');
     cy.get('.scan-button').click();
     cy.get('.error').contains('must be a valid product number');
   });
 
   it('should give error when entering non existent productnumber', () => {
-    login();
     cy.get('.scan-input').first().type('998899');
     cy.get('.scan-button').click();
     cy.get('.error').contains('Product not found');
   });
 
-  it('should return existing product', () => {
-    login();
-
-    cy.get('.scan-input').first().type('998899');
-    cy.get('.scan-button').click();
-    cy.get('.error').contains('Product not found');
+  it('should navigate to filter by customer', () => {
+    cy.get('.scan-input').first().type('ddddd');
+    cy.get('.FilterByCustomerButton').click();
+    cy.get('h2').contains('Customer List');
   });
-
 
 });
 
